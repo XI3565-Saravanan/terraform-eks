6458588f71bee31aad3cd566ee8b73fd69d1c804
XI3565-Saravanan <106864317+XI3565-Saravanan@users.noreply.github.com>
2022-06-15 11:28:14 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,9 @@
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-your_first_subnet_id"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-your_second_subnet_id"
+ }
