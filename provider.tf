0452c8d173bc3d8f1310b457c46eaa43af7c7c81
sachin199112 <107113435+sachin199112@users.noreply.github.com>
2022-06-17 22:55:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,4 +4,24 @@ terraform {
    source = "hashicorp/aws"
   }
  }
+  backend "s3" {
+    bucket         = "bcuket-name"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    #dynamodb_table = "dynamodb-table"
+  }
 }
+
+
+provider "aws" {
+  region = var.region
+}
+
+#terraform {
+#  backend "s3" {
+#    bucket         = "sachin-terraform"
+#    key            = "terraform/terraform.tfstate"
+#    region         = "us-east-1"
+#    dynamodb_table = "terraform-sachin"
+#  }
+#}
